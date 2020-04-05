e18a6f53733f29714870b6cade50d1abf5810f8a
Baguskurniawan04 <55921154+Baguskurniawan04@users.noreply.github.com>
2020-04-05 22:52:41 +0700

    Add files via upload

---
+++ simplerestandroid-master/web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id11935134_service');
+ define('PASS','12345678');
+ define('DB','id11935134_web');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
